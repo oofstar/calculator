ec51a697b35f2449463cf41e3c8c4ee463301a01
Al Fair <thealisonfair@gmail.com>
2019-01-15 19:36:53 -0500

    have array of numbers

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+// where you are now, 1/15/2019
+// you are trying to parse out the different numbers and operators from the displayValue so you can calculate them. You have an array that pulls out numbers but makes strings with the operators included.
+
+
+
+
 const testStrip = "this works";
 
 function add(a, b) {
@@ -42,9 +48,20 @@ function operate(operator, a, b) {
 }
 let displayValue = "";
 
+const operators = ["+", "-", "*", "/"];
 
 const display = document.getElementById("display");
 const numButtons = document.querySelectorAll(".numButton");
+const clearButton = document.getElementById("clearButton");
+const calculateButton = document.getElementById("calculateButton");
+
+calculateButton.addEventListener("click", (e) => {
+  calculate(displayValue);
+})
+
+clearButton.addEventListener("click", (e) => {
+  clearDisplay();
+})
 
 numButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -53,7 +70,42 @@ numButtons.forEach((button) => {
   });
 })
 
+function calculate(input) {
+  console.log(input);
+  // find operators
+  inputArray = input.split("");
+  let operatorIndices = [0];
+  inputArray.forEach((item) => {
+    if (operators.includes(item)) {
+      operatorIndices.push(inputArray.indexOf(item))
+    }
+  })
+
+  // create array for calculation
+  let calcArray = [];
+  operatorIndices.forEach((item, index) => {
+    // console.log(input.charAt(item));
+    console.log(item);
+    console.log(operatorIndices[index+1]);
+    calcArray.push(input.slice(item, operatorIndices[index+1]))
+
+  })
+
+  calcArray.forEach
+
+
+
+  // inputArray = input.split("");
+  // console.log(inputArray);
+
+}
+
 function buttonPress(value) {
   displayValue += value;
+  display.innerText = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "";
   display.innerText = displayValue;
 }
